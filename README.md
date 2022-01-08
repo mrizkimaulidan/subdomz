4052f27ed5c6ec47334bc30f009abc70134d4cba
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2022-01-08 11:59:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# subdomz
+
+Personal tools to enumerate subdomain.
+
+Clone :
+
+```bash
+$ git clone https://github.com/mrizkimaulidan/subdomz.git
+```
+
+Download the required modules :
+
+```bash
+$ pip3 install -r requirements.txt
+```
+
+Usage :
+
+```bash
+$ python3 script.py
+```
+
+```bash
+Normal Usage : script.py example.com
+Saving Output : script.py -o example.com
+```
+
+Tested on Kali Linux 2021.4
---
+++ script.py
@@ -0,0 +1,59 @@
+import requests
+import sys
+
+
+def check_arguments(arguments):
+    if arguments[1] == '-o':
+        return arguments[2], True
+    else:
+        return arguments[1], False
+
+
+class Subdomz:
+    URL = "https://crt.sh/?Identity={}&output=json"
+    RESULT = []
+
+    def __init__(self, url, output_name):
+        self.url = url
+        self.output_name = output_name
+
+    def fetch(self):
+        return requests.get(self.URL.format(self.url)).json()
+
+    def check_duplicate_values(self, response):
+        for r in response:
+            if r['common_name'] not in self.RESULT:
+                self.RESULT.append(r['common_name'])
+
+        return
+
+    def print(self):
+        for r in self.RESULT:
+            if self.output_name != False:
+                self.save_file()
+
+            print(r)
+
+        return
+
+    def save_file(self):
+        with open(f'{self.url}.txt', 'w+') as f:
+            for r in self.RESULT:
+                f.write(f'{r}\n')
+
+        return
+
+
+if __name__ == '__main__':
+    arguments = sys.argv
+
+    if len(arguments) <= 1:
+        print('Normal Usage : script.py <domain_url>')
+        print('Saving Output : script.py -o <domain_url>')
+    else:
+        url, is_output = check_arguments(arguments)
+
+        application = Subdomz(url, is_output)
+        response = application.fetch()
+        application.check_duplicate_values(response)
+        application.print()
